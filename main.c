6a5981924e01e4d21b5f5eb4f4ba16bf2b9c6ba5
Yassine-Abouayyoub <yasynabwaywb3@gmail.com>
2025-11-21 17:07:30 +0100

    Update main.c and test

--- main.c
+++ main.c
@@ -9,15 +9,16 @@ const char* welcome =
 "********************************************\n";
 
 const char* info =
-"Use this library to center any terminal line\n"
-"It works with English, numbers, symbols, etc.\n"
-"Just specify your actual line width for beauty\n";
+"**************************************************\n"
+"*  Use this library to center any terminal line  *\n"
+"*It works with characters, numbers, symbols, etc.*\n"
+"* Just specify your actual line width for beauty *\n"
+"**************************************************\n";
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8); // Enable international characters
+    SetConsoleOutputCP(CP_UTF8); // Enable Unicode
     printc(welcome, 44);         // Each line is 44 characters wide
-    printc(info, 40);            // Each line is about 40 characters
-    getchar(); // Wait for a key press before closing
+    printc(info, 50);            // Each line is 50 characters wide
+    getchar(); // Wait for key press before closing
     return 0;
-
 }
