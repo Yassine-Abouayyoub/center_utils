0b48b32b0bbf5499c81115b176f7a87d6903727d
Yassine Abouayyoub <yasynabwaywb3@gmail.com>
2025-11-21 20:10:07 +0100

    Refactor print functions for centered output

--- center_utils/center_utils.c
+++ center_utils/center_utils.c
@@ -1,32 +1,43 @@
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
+#include <stdarg.h>
 #include <string.h>
 #include "center_utils.h"
 
-void printCentered(const char* text, int width) {
+void printc(const char* fmt, ...) {
+    char buffer[512];
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    int maxlen = 0;
+    const char* ptr = buffer;
+    while (*ptr) {
+        const char* nl = strchr(ptr, '\n');
+        int len = nl ? (nl - ptr) : strlen(ptr);
+        if (len > maxlen) maxlen = len;
+        if (!nl) break;
+        ptr = nl + 1;
+    }
+
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     int columns = 80;
     if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
         columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
     }
-    int pad = (columns - width) / 2;
+    int pad = (columns - maxlen) / 2;
     if (pad < 0) pad = 0;
-    for (int i = 0; i < pad; i++) putchar(' ');
-    printf("%s\n", text);
-}
 
-void printc(const char* bigText, int fixed_width) {
-    char buffer[512];
-    const char* start = bigText;
-    while (*start) {
-        const char* end = strchr(start, '\n');
-        if (!end) end = start + strlen(start);
-        size_t line_len = end - start;
-        strncpy(buffer, start, line_len);
-        buffer[line_len] = '\0';
-        printCentered(buffer, fixed_width);
-
-        if (*end == '\n') start = end + 1;
-        else break;
+    ptr = buffer;
+    while (*ptr) {
+        const char* nl = strchr(ptr, '\n');
+        int len = nl ? (nl - ptr) : strlen(ptr);
+        for (int i = 0; i < pad; i++) putchar(' ');
+        fwrite(ptr, 1, len, stdout);
+        putchar('\n');
+        if (!nl) break;
+        ptr = nl + 1;
     }
 }
