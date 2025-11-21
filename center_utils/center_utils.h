d262b50be38062c67526951ec7a0df8de872a5be
Yassine Abouayyoub <yasynabwaywb3@gmail.com>
2025-11-21 20:10:33 +0100

    Refactor print functions in center_utils.h

--- center_utils/center_utils.h
+++ center_utils/center_utils.h
@@ -1,7 +1,6 @@
 #ifndef CENTER_UTILS_H
 #define CENTER_UTILS_H
 
-void printCentered(const char* text, int width);
-void printc(const char* bigText, int fixed_width);
+void printc(const char* fmt, ...);
 
 #endif
